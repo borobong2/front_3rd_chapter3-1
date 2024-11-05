d85245aba95edceed6206fdfc1953f2e75e31994
Bong <borobong230@gmail.com>
2024-11-06 00:25:38 +0900

    :white_check_mark: [Test] formatMonth 테스트 케이스 작성 #easy

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -220,7 +220,11 @@ describe('formatWeek', () => {
 });
 
 describe('formatMonth', () => {
-  it("2024년 7월 10일을 '2024년 7월'로 반환한다", () => {});
+  it("2024년 7월 10일을 '2024년 7월'로 반환한다", () => {
+    const date = new Date('2024-07-10');
+    const result = formatMonth(date);
+    expect(result).toBe('2024년 7월');
+  });
 });
 
 describe('isDateInRange', () => {
