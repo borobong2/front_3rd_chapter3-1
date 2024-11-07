62ccb869bc52986945981ed60e1e3c1676eff8f0
Bong <borobong230@gmail.com>
2024-11-08 02:34:44 +0900

    :recycle: [Refactor] eventFormSection 분리 #easy

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Box, Flex } from '@chakra-ui/react';
 
 import { AlertSection } from './components/AlertSection';
 import { CalendarSection } from './components/CalendarSection';
-import { EventFormSection } from './components/EventFormSection';
+import { EventForm } from './components/eventForm/EventForm.tsx';
 import { EventListSection } from './components/EventListSection';
 
 function App() {
   return (
     <Box w="full" h="100vh" m="auto" p={5}>
       <Flex gap={6} h="full">
-        <EventFormSection />
+        <EventForm />
         <CalendarSection />
         <EventListSection />
       </Flex>
